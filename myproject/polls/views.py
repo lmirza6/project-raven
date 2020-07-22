0c84c1e8e6ab6183ce6a384bef0869de8bae6d3e
Laila <laila.mirza@live.co.uk>
2020-07-22 17:07:20 +0100

    set base folder

--- myproject/polls/views.py
+++ myproject/polls/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Question 
 
-# Create your views here.
+
+def index(request):
+    context = {"questions": Question.objects.all()} 
+    return render(request, "polls/index.html", context)
