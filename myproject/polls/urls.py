d6d1c5a5a806cbdeae813f02cd8b3ffbe87c4cdf
emurphynanton@gmail.com <ethan@Ethans-MacBook-Pro.local>
2020-07-23 17:10:46 +0100

    Progress

--- myproject/polls/urls.py
+++ myproject/polls/urls.py
@@ -3,4 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name = "index"),
+    path('location/', views.location, name = "location"),
+    path('specLocation/', views.specLocation, name = "specLocation"),
+    path('event/', views.event, name = "event"),
+    path('date/', views.date, name = "date"),
 ]
+
+# urlpatterns = [
+#     path('', views.location, name = "location"),
+# ]
--- myproject/polls/views.py
+++ myproject/polls/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Report
-
+from .models import Location
 
 
 def index(request):
+    date = {"dates": Report.objects.all()}
     context = {"reports": Report.objects.all()} 
     return render(request, "polls/index.html", context)
+
+def location(request):
+    context = {"locations": Location.objects.all()} 
+    return render(request, "polls/location.html", context)
+
+def specLocation(request):
+    context = {"locations": Location.objects.all()}
+    return render(request, "polls/specLocation.html", context)
+
+def date(request):
+    context = {"dates": Report.objects.all()}
+    return render(request, "polls/date.html", context)
+
+def event(request):
+    context = {"event": Report.objects.all()}
+    return render(request, "polls/event.html", context)
